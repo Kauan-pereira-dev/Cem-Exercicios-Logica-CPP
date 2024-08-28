5e2a1dfebf664b7bad87684dff40944133ae45da
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-08-28 20:41:13 -0300

    build: ex02

---
+++ ex02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nome;
+    std::cout << "Digite o seu nome: \n";
+    std::cin >> nome;
+    std::cout << "Ola, " << nome << ". E um prazer te conhecer!\n";
+    return 0;
+
+}
