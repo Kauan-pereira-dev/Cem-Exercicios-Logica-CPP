752985452759de3f4e27300d2502ed493aed125b
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-08-28 21:16:24 -0300

    build: ex05

---
+++ ex05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double nota1, nota2, med;
+    std::cout << "Digite sua primeira nota: ";
+    std::cin >> nota1;
+    std::cout << "Digite sua segunda nota: ";
+    std::cin >> nota2; 
+    med = (nota1+nota2/2)  
+    std::cout << "A media das duas notas digitadas e: " << med;
+    return 0;
+}
